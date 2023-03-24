586bf88e6ae6bb3589075d767e7ce7b353a93e65
Grigory-Mag <magankov2003@mail.ru>
2023-03-24 16:30:23 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using ApiService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiService
+{
+    internal class Class1
+    {
+        DriversObject DriversObject = new DriversObject();
+    }
+}
